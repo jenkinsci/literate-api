f8b10beab5a36c7be14197623ff39da8b5971408
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-10-16 16:52:52 -0700

    ignoring NPE is a horrible idea since it can happen in builder.build(request)
    If the intent is to ignore broken ProjectModelBuilder, you need to catch  ServiceConfigurationError during ServiceLoader iteration

--- src/main/java/org/cloudbees/literate/api/v1/ProjectModelSource.java
+++ src/main/java/org/cloudbees/literate/api/v1/ProjectModelSource.java
@@ -31,6 +31,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.ServiceConfigurationError;
 import java.util.ServiceLoader;
 
 /**
@@ -94,8 +95,6 @@ public class ProjectModelSource {
                 if (pmbe == null) {
                     pmbe = e;
                 }
-            } catch (NullPointerException e) {
-                // ignore
             }
         }
         if (ioe != null) {
